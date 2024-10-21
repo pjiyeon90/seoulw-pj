35174e6b0dcd798d16549405d57e76c33da94671
yeon <parkwlfod@naver.com>
2024-10-21 11:53:28 +0900

    sdf

---
+++ src/components/Bookmark.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import bookmarkStyle from '@/styles/bookmark.module.scss'
+
+function Bookmark() {
+  return (
+    <div>Bookmark</div>
+  )
+}
+
+export default Bookmark
